dc2a2fe5c448ec3147a73724682ba41253f92587
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2024-03-10 07:42:27 +0600

    added id generation strategy

---
+++ src/main/java/org/ablonewolf/jpa/entities/Artist.java
@@ -0,0 +1,26 @@
+package org.ablonewolf.jpa.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "artists")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Artist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "artist_id")
+    private Integer id;
+    @Column(name = "artist_name")
+    private String name;
+
+    public Artist(String name) {
+        this.name = name;
+    }
+
+}
